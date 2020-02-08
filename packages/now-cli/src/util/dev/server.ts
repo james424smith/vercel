james424smith/vercel

commit ef4949e94abf6dbc40a4d118f1bedb5533a5acd1

[now-cli] Use npx when available to run dev command (#3749)
* use npx when available

* add --no-install

* replace $PORT and %PORT% in devCommand

* remove PORT from env variables

* replace more than one $PORT or %PORT%

* fix regex

packages/now-cli/src/util/dev/server.ts
@@ -17,6 +17,7 @@ import directoryTemplate from 'serve-handler/src/directory';
 import getPort from 'get-port';
 import { ChildProcess } from 'child_process';
 import isPortReachable from 'is-port-reachable';
+import which from 'which';
 
 import {
   Builder,
@@ -1651,24 +1652,36 @@ export default class DevServer {
     const env: EnvConfig = {
       ...process.env,
       ...this.buildEnv,
-      PATH: `${yarnBinPath}${delimiter}${process.env.PATH}`,
       NOW_REGION: 'dev1',
-      PORT: `${port}`,
     };
+
+    const devCommand = this.devCommand
+      .replace(/\$PORT/g, `${port}`)
+      .replace(/%PORT%/g, `${port}`);
 
     this.output.debug(
       `Starting dev command with parameters : ${JSON.stringify({
         cwd: this.cwd,
-        devCommand: this.devCommand,
+        devCommand,
         port,
       })}`
     );
 
-    const p = spawnCommand(this.devCommand, {
-      stdio: 'inherit',
-      cwd,
-      env,
-    });
+    const isNpxAvailable = await which('npx')
+      .then(() => true)
+      .catch(() => false);
+
+    if (!isNpxAvailable) {
+      env.PATH = `${yarnBinPath}${delimiter}${env.PATH}`;
+    }
+
+    this.output.debug('Spawning dev command');
+    this.output.debug(`PATH is ${env.PATH}`);
+
+    const p = spawnCommand(
+      isNpxAvailable ? `npx --no-install ${devCommand}` : devCommand,
+      { stdio: 'inherit', cwd, env }
+    );
 
     p.on('exit', () => {
       this.devProcessPort = undefined;

commit 2b025511a6c49e720e1bf684838da57172b4e2b9

[now-cli] Fix port assignment for `now dev` (#3769)
This fixes a regression from #3749 where the PORT env var was removed. This is necessary so frameworks like create-react-app and gatsby can proxy to `now dev`.

Fixes #3761

packages/now-cli/src/util/dev/server.ts
@@ -1632,12 +1632,14 @@ export default class DevServer {
   }
 
   async runDevCommand() {
-    if (!this.devCommand) return;
+    const { devCommand, cwd } = this;
 
-    const cwd = this.cwd;
+    if (!devCommand) {
+      return;
+    }
 
     this.output.log(
-      `Running Dev Command ${chalk.cyan.bold(`“${this.devCommand}”`)}`
+      `Running Dev Command ${chalk.cyan.bold(`“${devCommand}”`)}`
     );
 
     const port = await getPort();
@@ -1646,35 +1648,36 @@ export default class DevServer {
       ...process.env,
       ...this.buildEnv,
       NOW_REGION: 'dev1',
+      PORT: `${port}`,
     };
 
-    const devCommand = this.devCommand
+    // This is necesary so that the dev command in the Project
+    // will work cross-platform (especially Windows).
+    let command = devCommand
       .replace(/\$PORT/g, `${port}`)
       .replace(/%PORT%/g, `${port}`);
 
     this.output.debug(
       `Starting dev command with parameters : ${JSON.stringify({
-        cwd: this.cwd,
-        devCommand,
+        cwd,
+        command,
         port,
       })}`
     );
-
-    let command = devCommand;
 
     const isNpxAvailable = await which('npx')
       .then(() => true)
       .catch(() => false);
 
     if (isNpxAvailable) {
-      command = `npx --no-install ${devCommand}`;
+      command = `npx --no-install ${command}`;
     } else {
       const isYarnAvailable = await which('yarn')
         .then(() => true)
         .catch(() => false);
 
       if (isYarnAvailable) {
-        command = `yarn run --silent ${devCommand}`;
+        command = `yarn run --silent ${command}`;
       }
     }
 

packages/now-cli/test/dev/fixtures/04-create-react-app/.env
@@ -1,0 +1,1 @@
+SKIP_PREFLIGHT_CHECK=true

packages/now-cli/test/dev/fixtures/04-create-react-app/.gitignore
@@ -22,3 +22,4 @@ npm-debug.log*
 yarn-debug.log*
 yarn-error.log*
 !yarn.lock
+!.env

packages/now-cli/test/dev/integration.js
@@ -770,17 +770,17 @@ test('[now dev] 03-aurelia', async t => {
   await tester(t);
 });
 
-// test(
-//   '[now dev] 04-create-react-app-node',
-//   testFixtureStdio('create-react-app', async(t, port) => {
-//     const response = await fetch(`http://localhost:${port}`);
+test(
+  '[now dev] 04-create-react-app',
+  testFixtureStdio('04-create-react-app', async (t, port) => {
+    const response = await fetch(`http://localhost:${port}`);
 
-//     validateResponseHeaders(t, response);
+    validateResponseHeaders(t, response);
 
-//     const body = await response.text();
-//     t.regex(body, /React App/gm);
-//   })
-// );
+    const body = await response.text();
+    t.regex(body, /React App/gm);
+  })
+);
 
 test('[now dev] 05-gatsby', async t => {
   if (shouldSkip(t, '05-gatsby', '>^6.14.0 || ^8.10.0 || >=9.10.0')) return;
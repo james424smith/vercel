james424smith/vercel

commit 4c08d6538c27c4466a0fe0a7e6130343618ab5a6

[now-cli] Use `devCommand` from project settings locally (#3673)
* pull devCommand from cloud

* filter out front end builders with devCommand

* remove unnecessary import

* fix wrong directory listing with devCommand

* fix server not stopping

* Apply suggestions from code review

Co-Authored-By: Nathan Rajlich <n@n8.io>

* remove comment

* remove redundant "if"

Co-authored-by: Nathan Rajlich <n@n8.io>

packages/now-cli/src/util/dev/server.ts
@@ -11,9 +11,12 @@ import { randomBytes } from 'crypto';
 import serveHandler from 'serve-handler';
 import { watch, FSWatcher } from 'chokidar';
 import { parse as parseDotenv } from 'dotenv';
-import { basename, dirname, extname, join } from 'path';
+import { basename, dirname, extname, join, delimiter } from 'path';
 import { getTransformedRoutes, HandleValue } from '@now/routing-utils';
 import directoryTemplate from 'serve-handler/src/directory';
+import getPort from 'get-port';
+import { ChildProcess } from 'child_process';
+import isPortReachable from 'is-port-reachable';
 
 import {
   Builder,
@@ -23,6 +26,8 @@ import {
   detectRoutes,
   detectApiDirectory,
   detectApiExtensions,
+  execAsync,
+  spawnCommand,
 } from '@now/build-utils';
 
 import { once } from '../once';
@@ -118,6 +123,9 @@ export default class DevServer {
   private watchAggregationTimeout: number;
   private filter: (path: string) => boolean;
   private podId: string;
+  private devCommand?: string;
+  private devProcess?: ChildProcess;
+  private devProcessPort?: number;
 
   private getNowConfigPromise: Promise<NowConfig> | null;
   private blockingBuildsPromise: Promise<void> | null;
@@ -131,6 +139,7 @@ export default class DevServer {
     this.buildEnv = {};
     this.files = {};
     this.address = '';
+    this.devCommand = options.devCommand;
 
     // This gets updated when `start()` is invoked
     this.yarnPath = '/';
@@ -525,7 +534,7 @@ export default class DevServer {
       const featHandleMiss = true; // enable for zero config
       const { projectSettings, cleanUrls, trailingSlash } = config;
 
-      const { builders, warnings, errors } = await detectBuilders(files, pkg, {
+      let { builders, warnings, errors } = await detectBuilders(files, pkg, {
         tag: getDistTag(cliVersion) === 'canary' ? 'canary' : 'latest',
         functions: config.functions,
         ...(projectSettings ? { projectSettings } : {}),
@@ -541,6 +550,10 @@ export default class DevServer {
       }
 
       if (builders) {
+        if (this.devCommand) {
+          builders = builders.filter(filterFrontendBuilds);
+        }
+
         const {
           defaultRoutes,
           redirectRoutes,
@@ -571,10 +584,14 @@ export default class DevServer {
     }
 
     if (!config.builds || config.builds.length === 0) {
-      if (isInitialLoad) {
+      if (isInitialLoad && !this.devCommand) {
         this.output.note(`Serving all files as static`);
       }
     } else if (Array.isArray(config.builds)) {
+      if (this.devCommand) {
+        config.builds = config.builds.filter(filterFrontendBuilds);
+      }
+
       // `@now/static-build` needs to be the last builder
       // since it might catch all other requests
       config.builds.sort(sortBuilders);
@@ -806,6 +823,8 @@ export default class DevServer {
     // Wait for "ready" event of the watcher
     await once(this.watcher, 'ready');
 
+    const devCommandPromise = this.runDevCommand();
+
     let address: string | null = null;
     while (typeof address !== 'string') {
       try {
@@ -837,6 +856,8 @@ export default class DevServer {
       .replace('[::]', 'localhost')
       .replace('127.0.0.1', 'localhost');
 
+    await devCommandPromise;
+
     this.output.ready(`Available at ${link(this.address)}`);
     this.serverUrlPrinted = true;
   }
@@ -860,6 +881,18 @@ export default class DevServer {
     for (const match of this.buildMatches.values()) {
       ops.push(shutdownBuilder(match, this.output));
     }
+
+    ops.push(
+      new Promise(resolve => {
+        if (!this.devProcess) {
+          resolve();
+          return;
+        }
+
+        this.devProcess.on('exit', () => resolve());
+        process.kill(this.devProcess.pid, exitCode);
+      })
+    );
 
     ops.push(close(this.server));
 
@@ -1291,6 +1324,18 @@ export default class DevServer {
     const requestPath = dest.replace(/^\//, '');
 
     if (!match) {
+      // if the dev command is started, proxy to it
+      if (this.devProcessPort) {
+        this.output.debug('Proxy to dev command server');
+        return proxyPass(
+          req,
+          res,
+          `http://localhost:${this.devProcessPort}`,
+          this.output,
+          false
+        );
+      }
+
       if (
         (statusCode === 404 && routeResult.phase === 'miss') ||
         !this.renderDirectoryListing(req, res, requestPath, nowRequestId)
@@ -1348,6 +1393,18 @@ export default class DevServer {
     }
 
     if (!foundAsset) {
+      // if the dev command is started, proxy to it
+      if (this.devProcessPort) {
+        this.output.debug('Proxy to dev command server');
+        return proxyPass(
+          req,
+          res,
+          `http://localhost:${this.devProcessPort}`,
+          this.output,
+          false
+        );
+      }
+
       await this.send404(req, res, nowRequestId);
       return;
     }
@@ -1547,6 +1604,55 @@ export default class DevServer {
     }
     return false;
   }
+
+  async runDevCommand() {
+    if (!this.devCommand) return;
+
+    const cwd = this.cwd;
+
+    const { stdout: yarnBinStdout } = await execAsync('yarn', ['bin'], {
+      cwd,
+    });
+
+    const yarnBinPath = yarnBinStdout.trim();
+
+    this.output.log(
+      `Running Dev Command ${chalk.cyan.bold(`“${this.devCommand}”`)}`
+    );
+
+    const port = await getPort();
+
+    const env: EnvConfig = {
+      ...process.env,
+      ...this.buildEnv,
+      PATH: `${yarnBinPath}${delimiter}${process.env.PATH}`,
+      NOW_REGION: 'dev1',
+      PORT: `${port}`,
+    };
+
+    this.output.debug(
+      `Starting dev command with parameters : ${JSON.stringify({
+        cwd: this.cwd,
+        devCommand: this.devCommand,
+        port,
+      })}`
+    );
+
+    const p = spawnCommand(this.devCommand, {
+      stdio: 'inherit',
+      cwd,
+      env,
+    });
+
+    p.on('exit', () => {
+      this.devProcessPort = undefined;
+    });
+
+    await checkForPort(port, 1000 * 60 * 5);
+
+    this.devProcessPort = port;
+    this.devProcess = p;
+  }
 }
 
 /**
@@ -1556,13 +1662,14 @@ function proxyPass(
   req: http.IncomingMessage,
   res: http.ServerResponse,
   dest: string,
-  output: Output
+  output: Output,
+  ignorePath: boolean = true
 ): void {
   const proxy = httpProxy.createProxyServer({
     changeOrigin: true,
     ws: true,
     xfwd: true,
-    ignorePath: true,
+    ignorePath,
     target: dest,
   });
 
@@ -1800,4 +1907,28 @@ function fileRemoved(
 function needsBlockingBuild(buildMatch: BuildMatch): boolean {
   const { builder } = buildMatch.builderWithPkg;
   return typeof builder.shouldServe !== 'function';
+}
+
+async function sleep(n: number) {
+  return new Promise(resolve => setTimeout(resolve, n));
+}
+
+async function checkForPort(
+  port: number | undefined,
+  timeout: number
+): Promise<void> {
+  const start = Date.now();
+  while (!(await isPortReachable(port))) {
+    if (Date.now() - start > timeout) {
+      throw new Error(`Detecting port ${port} timed out after ${timeout}ms`);
+    }
+    await sleep(100);
+  }
+}
+
+function filterFrontendBuilds(build: Builder) {
+  return (
+    !build.use.startsWith('@now/static-build') &&
+    !build.use.startsWith('@now/next')
+  );
 }

packages/now-cli/@types/is-port-reachable/index.d.ts
@@ -1,0 +1,10 @@
+declare module 'is-port-reachable' {
+  export interface IsPortReachableOptions {
+    timeout?: number | undefined;
+    host?: string;
+  }
+  export default function(
+    port: number | undefined,
+    options?: IsPortReachableOptions
+  ): Promise<boolean>;
+}

packages/now-cli/src/commands/dev/dev.ts
@@ -4,6 +4,10 @@ import DevServer from '../../util/dev/server';
 import parseListen from '../../util/dev/parse-listen';
 import { Output } from '../../util/output';
 import { NowContext } from '../../types';
+import Client from '../../util/client';
+import { getLinkedProject } from '../../util/projects/link';
+import { getFrameworks } from '../../util/get-frameworks';
+import { isSettingValue } from '../../util/is-setting-value';
 
 type Options = {
   '--debug'?: boolean;
@@ -20,7 +24,38 @@ export default async function dev(
   const cwd = path.resolve(dir);
   const listen = parseListen(opts['--listen'] || '3000');
   const debug = opts['--debug'] || false;
-  const devServer = new DevServer(cwd, { output, debug });
+
+  const client = new Client({
+    apiUrl: ctx.apiUrl,
+    token: ctx.authConfig.token,
+    currentTeam: ctx.config.currentTeam,
+    debug,
+  });
+
+  // retrieve dev command
+  const [[, project], frameworks] = await Promise.all([
+    getLinkedProject(output, client, cwd),
+    getFrameworks(),
+  ]);
+
+  let devCommand: undefined | string;
+  if (project) {
+    if (project.devCommand) {
+      devCommand = project.devCommand;
+    } else if (project.framework) {
+      const framework = frameworks.find(f => f.slug === project.framework);
+
+      if (framework) {
+        const defaults = framework.settings.devCommand;
+
+        if (isSettingValue(defaults)) {
+          devCommand = defaults.value;
+        }
+      }
+    }
+  }
+
+  const devServer = new DevServer(cwd, { output, debug, devCommand });
 
   process.once('SIGINT', () => devServer.stop());
 

packages/now-cli/src/types.ts
@@ -222,6 +222,8 @@ export interface Project {
   accountId: string;
   updatedAt: number;
   createdAt: number;
+  devCommand?: string | null;
+  framework?: string | null;
 }
 
 export interface Org {

packages/now-cli/src/util/dev/types.ts
@@ -18,6 +18,7 @@ export { NowConfig };
 export interface DevServerOptions {
   output: Output;
   debug: boolean;
+  devCommand?: string;
 }
 
 export interface EnvConfig {

packages/now-cli/src/util/get-frameworks.ts
@@ -1,0 +1,14 @@
+import fetch from 'node-fetch';
+import { Framework } from '@now/frameworks';
+
+export async function getFrameworks(): Promise<Framework[]> {
+  const res = await fetch('https://api-frameworks.zeit.sh/api/frameworks');
+
+  if (!res.ok) {
+    throw new Error('Could not retrieve frameworks');
+  }
+
+  const json: Framework[] = await res.json();
+
+  return json;
+}

packages/now-cli/src/util/input/edit-project-settings.ts
@@ -2,7 +2,8 @@ import inquirer from 'inquirer';
 import confirm from './confirm';
 import chalk from 'chalk';
 import { Output } from '../output';
-import { Framework, SettingValue } from '@now/frameworks';
+import { Framework } from '@now/frameworks';
+import { isSettingValue } from '../is-setting-value';
 
 export interface ProjectSettings {
   buildCommand: string | null;
@@ -15,10 +16,6 @@ const fields: { name: string; value: keyof ProjectSettings }[] = [
   { name: 'Output Directory', value: 'outputDirectory' },
   { name: 'Development Command', value: 'devCommand' },
 ];
-
-function isSettingValue(setting: any): setting is SettingValue {
-  return setting && typeof setting.value === 'string';
-}
 
 export default async function editProjectSettings(
   output: Output,

packages/now-cli/src/util/is-setting-value.ts
@@ -1,0 +1,5 @@
+import { SettingValue } from '@now/frameworks';
+
+export function isSettingValue(setting: any): setting is SettingValue {
+  return setting && typeof setting.value === 'string';
+}

commit 15c290535e42d4ae50668f314cd49ff00b0ef769

[cli] Fix `vc dev` proxy to upstream frontend dev server (#4644)
This PR fixes a longstanding issue introduced in #3673 that prevents routing properties from applying to the framework's upstream dev server.

This mimic's the older proxy logic used in build matches here: https://github.com/vercel/vercel/blob/5035fa537f82181649d42778c60145d8890f3291/packages/now-cli/src/util/dev/server.ts#L1535-L1539

- Related to #3777
- Related to #4510

packages/now-cli/src/util/dev/server.ts
@@ -1503,16 +1503,15 @@ export default class DevServer {
     if (!match) {
       // If the dev command is started, then proxy to it
       if (this.devProcessPort) {
-        debug('Proxying to frontend dev server');
+        const upstream = `http://localhost:${this.devProcessPort}`;
+        debug(`Proxying to frontend dev server: ${upstream}`);
         this.setResponseHeaders(res, nowRequestId);
-        return proxyPass(
-          req,
-          res,
-          `http://localhost:${this.devProcessPort}`,
-          this,
-          nowRequestId,
-          false
-        );
+        const origUrl = url.parse(req.url || '/', true);
+        delete origUrl.search;
+        origUrl.pathname = dest;
+        Object.assign(origUrl.query, uri_args);
+        req.url = url.format(origUrl);
+        return proxyPass(req, res, upstream, this, nowRequestId, false);
       }
 
       if (

packages/now-cli/test/dev/fixtures/06-gridsome/src/pages/404.vue
@@ -1,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Not Found</h1>
+    <p>This is a Custom Gridsome 404.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Not Found'
+  }
+}
+</script>

packages/now-cli/test/dev/fixtures/06-gridsome/src/pages/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <Layout>
-    
+
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
+
+    <h1>Gridsome on Vercel</h1>
+
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>

packages/now-cli/test/dev/fixtures/07-hexo-node/source/contact.html
@@ -1,0 +1,1 @@
+<h1>Contact Us</h1>

packages/now-cli/test/dev/fixtures/10-nextjs-node/pages/contact.js
@@ -1,0 +1,3 @@
+export default function Contact() {
+  return <h1>Contact Page</h1>;
+}

packages/now-cli/test/dev/integration.js
@@ -1089,6 +1089,13 @@ test(
   '[vercel dev] 06-gridsome',
   testFixtureStdio('06-gridsome', async testPath => {
     await testPath(200, '/');
+    await testPath(200, '/about');
+    await testPath(308, '/support', 'Redirecting to /about?ref=support (308)', {
+      Location: '/about?ref=support',
+    });
+    // Bug with gridsome's dev server: https://github.com/gridsome/gridsome/issues/831
+    // Works in prod only so leave out for now
+    // await testPath(404, '/nothing');
   })
 );
 
@@ -1096,6 +1103,9 @@ test(
   '[vercel dev] 07-hexo-node',
   testFixtureStdio('07-hexo-node', async testPath => {
     await testPath(200, '/', /Hexo \+ Node.js API/m);
+    await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
+    await testPath(200, '/contact.html', /Contact Us/m);
+    await testPath(200, '/support', /Contact Us/m);
   })
 );
 
@@ -1120,6 +1130,8 @@ test(
   testFixtureStdio('10-nextjs-node', async testPath => {
     await testPath(200, '/', /Next.js \+ Node.js API/m);
     await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
+    await testPath(200, '/contact', /Contact Page/);
+    await testPath(200, '/support', /Contact Page/);
     await testPath(404, '/nothing', /Custom Next 404/);
   })
 );
@@ -1130,6 +1142,7 @@ test(
     '12-polymer-node',
     async testPath => {
       await testPath(200, '/', /Polymer \+ Node.js API/m);
+      await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
     },
     { skipDeploy: true }
   )
@@ -1141,6 +1154,7 @@ test(
     '13-preact-node',
     async testPath => {
       await testPath(200, '/', /Preact/m);
+      await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
     },
     { skipDeploy: true }
   )
@@ -1152,6 +1166,7 @@ test(
     '14-svelte-node',
     async testPath => {
       await testPath(200, '/', /Svelte/m);
+      await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
     },
     { skipDeploy: true }
   )
@@ -1163,6 +1178,7 @@ test(
     '16-vue-node',
     async testPath => {
       await testPath(200, '/', /Vue.js \+ Node.js API/m);
+      await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
     },
     { skipDeploy: true }
   )
@@ -1174,6 +1190,7 @@ test(
     '17-vuepress-node',
     async testPath => {
       await testPath(200, '/', /VuePress \+ Node.js API/m);
+      await testPath(200, '/api/date', new RegExp(new Date().getFullYear()));
     },
     { skipDeploy: true }
   )